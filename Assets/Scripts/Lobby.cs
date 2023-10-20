0b583ada1cce1d447404804d7d7a459a40c663a3
Roderick Vella Galea <roderick.vella@mcast.edu.mt>
2023-10-20 15:20:13 +0200

    Lesson4

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lobby : MonoBehaviourPunCallbacks
 {
@@ -31,6 +32,10 @@ public class Lobby : MonoBehaviourPunCallbacks
     [Tooltip("Panel waiting for players")]
     public GameObject PanelWaitingForPlayers;
 
+    List<RoomInfo> availableRooms = new List<RoomInfo>();
+
+    UnityEngine.Events.UnityAction buttonCallback;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,6 +52,9 @@ public class Lobby : MonoBehaviourPunCallbacks
             //connect to the photon master server
             PhotonNetwork.ConnectUsingSettings();
         }
+
+        InputRoomName.GetComponent<TMP_InputField>().text = "Room1";
+        InputPlayerName.GetComponent<TMP_InputField>().text = "Player1";
     }
 
     public override void OnConnectedToMaster()
@@ -55,6 +63,71 @@ public class Lobby : MonoBehaviourPunCallbacks
         //after we are connected to the master server, we need to connect to the lobby
         PhotonNetwork.JoinLobby(TypedLobby.Default);
 
+    }
+
+    public void CreateRoom()
+    {
+        //creating settings for our room
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 2;
+
+        //create room
+        PhotonNetwork.JoinOrCreateRoom(InputRoomName.GetComponent<TMP_InputField>().text,
+            roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        print("Created Room");
+        //set our player name
+        PhotonNetwork.NickName = InputPlayerName.GetComponent<TMP_InputField>().text;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        print("Cannot create room: Error Code:" + returnCode + "; Message: " + message);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+       
+    {
+        print("Number of Rooms in Lobby" + roomList.Count);
+        availableRooms = roomList;
+        if (roomList.Count > 0) InputPlayerName.GetComponent<TMP_InputField>().text = "Player2";
+        UpdateRoomList();
+    }
+
+    private void UpdateRoomList()
+    {
+        foreach(RoomInfo roomInfo in availableRooms)
+        {
+            GameObject rowRoom = Instantiate(RowRoom);
+            //attach it to the scrollview
+            rowRoom.transform.parent = ScrollViewContent.transform;
+            rowRoom.transform.localScale = Vector3.one;
+
+            //now we need to update the room name and number of players
+            rowRoom.transform.Find("RoomName").GetComponent<TextMeshProUGUI>().text = roomInfo.Name;
+            rowRoom.transform.Find("RoomPlayers").GetComponent<TextMeshProUGUI>().text =
+                roomInfo.PlayerCount.ToString();
+
+            //we need to make the join button clickable
+            buttonCallback = () => OnClickJoinRoom(roomInfo.Name);
+            rowRoom.transform.Find("BtnJoin").GetComponent<Button>()
+                .onClick.AddListener(buttonCallback);
+        }
+    }
+
+    public void OnClickJoinRoom(string roomName)
+    {
+        //set our player name
+        PhotonNetwork.NickName = InputPlayerName.GetComponent<TMP_InputField>().text;
+
+        //join the room by room name
+        
+        PhotonNetwork.JoinRoom(roomName);
     }
 
     private void OnGUI()
